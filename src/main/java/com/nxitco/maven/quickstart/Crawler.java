e5d68ef09d5519c9113ea3da21d7fd18612504a6
Nick Xitco <nxitco1@jhu.edu>
2017-12-12 18:46:20 -0500

    More database updating

--- src/main/java/com/nxitco/maven/quickstart/Crawler.java
+++ src/main/java/com/nxitco/maven/quickstart/Crawler.java
@@ -23,7 +23,7 @@ import com.wrapper.spotify.models.ClientCredentials;
  */
 public class Crawler 
 {
-	public static final int DATABASE_SIZE = 5000;
+	public static final int DATABASE_SIZE = 50000;
 	public static final String clientId = "5b00769425ca43019b6072c9fe842472";
 	public static final String clientSecret = "cc91b1e3ee824e0a8e94d64b5c5201b3";
 	private static final String DEFAULT_ID = "6eUKZXaKkcviH0Ku9w2n3V";
